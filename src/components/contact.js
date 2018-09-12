52b95ea78f8c76d48dca62864bd3d89fc1a8c4da
abhinavjonnada82 <abhinav.jonnada@gmail.com>
2018-09-11 21:34:09 -0500

    Just do it

--- src/components/contact.js
+++ src/components/contact.js
@@ -14,11 +14,7 @@ class Contact extends Component {
                      style={{height: '250px'}}
                      />
 
-                     <p style={{ width: '75%', margin: 'auto', paddingTop: '1em'}}>Prepared do an dissuade be so whatever steepest. Yet her beyond looked either 
-                         day wished nay. By doubtful disposed do juvenile an. Now curiosity 
-                         you explained immediate why behaviour. An dispatched impossible of of 
-                         melancholy favourable. Our quiet not heart along scale sense timed. Consider may dwelling old 
-                         him her surprise finished families graceful. Gave led past poor met fine was new. 
+                     <p style={{ width: '75%', margin: 'auto', paddingTop: '1em'}}>Just do it.
                      </p>
                  </Cell>
                  <Cell col={6}>
